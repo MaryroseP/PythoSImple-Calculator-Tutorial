e075e9ca278432eefdf1ec9c6cb1de355b10fdc1
Maryrose Pergis <mrpergis@student.apc.edu.ph>
2024-01-28 22:13:35 +0800

    Added separate constants.py file for variables and added color

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from constants import *
 
 
 class Calculator:
@@ -7,6 +8,20 @@ class Calculator:
         self.window.geometry("375x667")
         self.window.resizable(0,0)
         self.window.title("Calculator")
+
+        # Adding some display features and some buttons
+        self.display_frame = self.create_display_frame()
+        self.buttons_frame = self.create_button_frame()
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=disp_h, bg=light_pink)
+        frame.pack(expand=True, fill="both")
+        return frame
+    def create_button_frame(self):
+        frame = tk.Frame(self.window, bg=but_color)
+        frame.pack(expand=True, fill="both")
+        return frame
+
 
 
     def run(self):
---
+++ constants.py
@@ -0,0 +1,4 @@
+disp_h = 221
+
+light_pink = "#FFCCE6"
+but_color = "#D9649F"
